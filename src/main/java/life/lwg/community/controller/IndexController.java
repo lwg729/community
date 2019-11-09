3c3e70c0fe03e8aaa0d619ac2eb72c30bac19088
lwg729 <1098372951@qq.com>
2019-11-09 20:43:55 +0800

    âgit index and bookstrap

---
+++ src/main/java/life/lwg/community/controller/IndexController.java
@@ -0,0 +1,14 @@
+package life.lwg.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/index")
+    public String GreetingHello(){
+                return "index";
+    }
+
+}
