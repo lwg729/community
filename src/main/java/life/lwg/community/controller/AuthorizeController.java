0a6a3f3409cada17221df6eae3090821baa699a6
lwg729 <1098372951@qq.com>
2019-11-11 20:40:02 +0800

    git 配ébu 配置 from githubç

--- src/main/java/life/lwg/community/controller/AuthorizeController.java
+++ src/main/java/life/lwg/community/controller/AuthorizeController.java
@@ -4,24 +4,30 @@ import life.lwg.community.dto.AccessTokenDTO;
 import life.lwg.community.dto.GithubUser;
 import life.lwg.community.provider.GithubPorvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class AuthorizeController {
     @Autowired
     private GithubPorvider githubPorvider;
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientRedirect;
+    @Value("${github.redirect.url}")
+    private String redirectUrl;
+
     @GetMapping("/callback")
-    @ResponseBody
     public String callback(@RequestParam(name="code") String code,
                            @RequestParam(name="state")String state){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setClient_id("f30ffb702332e2e9ac57");
-        accessTokenDTO.setClient_secret("c0c3b7967f8c04a139708130c7bf0d958794a54d");
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientRedirect);
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri("http://localhost:8081/callback");
+        accessTokenDTO.setRedirect_uri(redirectUrl);
         accessTokenDTO.setState(state);
         String accessToken = githubPorvider.getAccessToken(accessTokenDTO);
         GithubUser user = githubPorvider.getUser(accessToken);
